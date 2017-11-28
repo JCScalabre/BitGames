c35e7e1958e8928807b8457bc290781ae18bbd0f
JC Scalabre <jc_scalabre@hotmail.com>
2017-11-28 10:10:38 -0600

    added instructions + a bit of styling

--- client/src/pages/MemoryTilesPage/MemoryTilesPage.js
+++ client/src/pages/MemoryTilesPage/MemoryTilesPage.js
@@ -13,6 +13,7 @@ class MemoryTilesPage extends Component {
 			<Link to="/"><button className="btn btn-primary">Go Home</button></Link>
 			<Link to="/leaderboard"><button className="btn btn-primary">Go To Leaderboard</button></Link>
 			<h3>Memory Tiles</h3>
+			<h5>Instructions: A random pattern will appear for 5 seconds. Try your best to memorize it and recreate it. When you're ready, press Start to begin!</h5>
 			<MemoryTiles/>
 			</div>
 			);
