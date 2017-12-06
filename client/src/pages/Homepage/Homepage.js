e09f9ca087e42d7891c3ec818869b1a6a984ff82
JC Scalabre <jc_scalabre@hotmail.com>
2017-12-05 19:42:41 -0600

    Final?

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -8,18 +8,24 @@ class Homepage extends Component {
 		return (
 			<div className="container">
 				<h3 className="bitgames">BITGAMES</h3>
+
 				<div className="row">
-					<div className="col-6 mx-auto text-center">
+					<div className="col">
 						<Link to="/memorytiles">
-							<button className="btn homebtn btn-primary">
-								Play Memory Tiles
-							</button>
+							<img id="bitgamesimage" src="images/bitgames.png" />
 						</Link>
-						<Link to="/leaderboard">
-							<button className="btn homebtn btn-primary">
-								View Leaderboard
-							</button>
-						</Link>
+						<div className="col-6">
+							<Link to="/memorytiles">
+								<button className="btn homebtn btn-primary">
+									Play Memory Tiles
+								</button>
+							</Link>
+							<Link to="/leaderboard">
+								<button className="btn homebtn btn-primary">
+									View Leaderboard
+								</button>
+							</Link>
+						</div>
 					</div>
 				</div>
 			</div>
